f5ca311a9fc9a69c79dc181cb68db75ecb6f2ac3
kayuho <724412707@qq.com>
2021-07-02 14:09:52 +0800

    增加date函数注释

---
+++ README.md
@@ -0,0 +1,7 @@
+PHPer 转 Goper 常用函数包
+
+## Date
+```
+// 输出当前时间
+fmt.Println(date.Date("2006/01/02 15:03:04"))
+```
--- date/date.go
+++ date/date.go
@@ -4,6 +4,11 @@ import (
 	"time"
 )
 
+// @title Date
+// @descripion 输出指定格式字符串时间，默认当前时间
+// @param format string "格式化 2006/01/02 15:03:04"
+// @param timestamps int64 "需要转化的时间戳"
+// @return string "格式化后的时间字符串"
 func Date(format string, timestamps ...int64) string {
 	var timestamp int64
 	if len(timestamps) == 0 {
